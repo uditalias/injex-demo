7ba24bd6df161d361896bc23761ca966a4b353f0
Udi Talias <udi.talias@gmail.com>
2021-03-16 10:26:36 +0200

    fix(config): https://github.com/uditalias/injex/issues/16

--- src/dog.ts
+++ src/dog.ts
@@ -5,7 +5,17 @@ import { Person } from "./person";
 @define()
 @singleton()
 export class Dog implements Animal {
-  @injectAlias("Person") private owner!: Person;
+
+  // Using `inject` instead of `injectAlias`
+  // The `injectAlias` is for cases where you have many classes
+  // with the same `@alias(NAME)` and you want to inject them into
+  // a keyd object or an array.
+  // Also, I'm passing the Person class and not the name "Person".
+  // Note that `@inject()` can be set with the module name or it's Type,
+  // since the Person module name is "person", you can choose between Person or "person"
+  @inject(Person) private owner!: Person; // Works
+  //@inject("person") private owner!: Person; // Works
+  //@inject("Person") private owner!: Person; // Won't work
 
   sleep() {
     return true;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import { Injex } from "@injex/node";
 
-Injex.create({ rootDirs: [__dirname] }).bootstrap();
+Injex.create({
+    rootDirs: [__dirname],
+
+    // Changed the `globPattern` since ts-node running
+    // the ts version of the files.
+    globPattern: "/**/*.ts",
+}).bootstrap();
